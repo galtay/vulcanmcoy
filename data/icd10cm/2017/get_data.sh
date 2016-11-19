dd5d1512e05dc9d8db9a658aad0b30caa8ed1c02
Gabriel <gabriel.altay@gmail.com>
2016-11-19 17:39:15 -0500

    download and read icd10cm codes

---
+++ data/icd10cm/2017/get_data.sh
@@ -0,0 +1,8 @@
+wget -nc https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-ICD10-Code-Descriptions.zip
+wget -nc https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-ICD10-Code-Tables-Index.zip
+wget -nc https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-GEM-DC.zip
+wget -nc https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2017-ICD-10-CM-Guidelines.pdf
+
+unzip -o -d code_table_index 2017-ICD10-Code-Tables-Index.zip
+unzip -o -d code_descriptions 2017-ICD10-Code-Descriptions.zip
+unzip -o -d gems_dc 2017-GEM-DC.zip
